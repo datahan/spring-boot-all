39cfb80686a73657a5a3c6bdf5a842ad2ce94d56
jing.han <jing.han@ezhiyang.com>
2019-08-15 20:33:17 +0800

    helloworld

---
+++ spring-boot-all-helloworld/src/main/java/com/datahan/springbootallhelloworld/SpringBootAllHelloworldApplication.java
@@ -0,0 +1,27 @@
+package com.datahan.springbootallhelloworld;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAllHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAllHelloworldApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, name = "who") String who) {
+
+        if (StringUtils.isBlank(who)) {
+            who = "World";
+        }
+
+        return "Hello, " + who + "!" ;
+    }
+}
---
+++ spring-boot-all-helloworld/src/test/java/com/datahan/springbootallhelloworld/SpringBootAllHelloworldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.datahan.springbootallhelloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAllHelloworldApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
