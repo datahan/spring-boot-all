4a851d708ba31335e640dc98e6979f7858b6df76
datahan <datawisher@gmail.com>
2019-10-20 23:05:06 +0800

    加入security

---
+++ spring-boot-all-security/src/main/java/com/datahan/springbootall/security/SpringBootAllSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.datahan.springbootall.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAllSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAllSecurityApplication.class, args);
+    }
+
+}
---
+++ spring-boot-all-security/src/main/java/com/datahan/springbootall/security/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.datahan.springbootall.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("user").password(new BCryptPasswordEncoder().encode("123")).roles("USER").and()
+                .withUser("admin").password(new BCryptPasswordEncoder().encode("456")).roles("USER", "ADMIN");
+    }
+}
